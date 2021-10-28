441bd338504e3a9b60f1cac16ef2b0e21ce7c5e2
Fabiano Heizer <fabianoheizer@MacBook-Pro-de-Fabiano.local>
2021-10-27 23:00:28 -0300

    initial commit

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+    //courses: Course[] = [];
+    courses$: Observable<Course[]>;
+    displayedColumns = ['name', 'category'];
+
+    constructor(private courseService: CoursesService) { 
+        this.courses$ = this.courseService.list();
+        
+        //this.courseService.list().subscribe(courses => this.courses = courses);
+
+    }
+
+    ngOnInit(): void {
+    }
+
+}
