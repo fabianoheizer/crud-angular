443412e999126be6ad3ec340a19e0875a7276710
fabianoheizer <fabianoheizer@gmail.com>
2021-10-27 23:48:46 -0300

    atualizacao do servico

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -9,9 +9,7 @@ export class CoursesService {
 
     private readonly API = '/assets/courses.json';
 
-    constructor(
-        private httpClient: HttpClient
-    ) { }
+    constructor(private httpClient: HttpClient) { }
 
     list(){
         return this.httpClient.get<Course[]>(this.API)
